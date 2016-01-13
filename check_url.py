54a392bda4f29a66d212a42684575b3ca970c43d
Narcissist1 <m6106918@gmail.com>
2016-01-13 17:09:18 +0800

    run method

--- check_url.py
+++ check_url.py
@@ -3,16 +3,16 @@
 # author-Zack
 
 import threading
-import os
 import Queue
 from datetime import datetime as dt
 from urlparse import urlparse
 from bs4 import BeautifulSoup
 import time
 
-TARGET_URL = "http://m.sohu.com/"
-WORKQUEUE = Queue.Queue()
-VISITED = {}
+TARGET_URL = "http://m.sohu.com/"  	# 目标url
+WORKQUEUE = Queue.Queue()  			# 待处理队列
+VISITED = {}  						# url是否处理过
+THREADNUM = 5  						# 线程数
 
 
 # 设置log文件
@@ -32,7 +32,7 @@ def get_logger(rooturl):
 
 
 # 广搜函数
-def bfs_path(url):
+def bfs_path(url,logger):
 	sonlinks = []
 	user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)'
 	headers = { 'User-Agent' : user_agent }
@@ -47,11 +47,11 @@ def bfs_path(url):
 			    sonlinks.append(templink)
 	except urllib2.HTTPError as e:
 		# HTTPError 需要在前面
-		self.logger.error('HTTPError:'+str(e.code)+'\n'+str(e.read())+url)
+		logger.error('HTTPError:'+str(e.code)+'\n'+str(e.read())+url)
 	except urllib2.URLError as e:
-		self.logger.error('URLError:'+str(e.reason)+url)
+		logger.error('URLError:'+str(e.reason)+url)
 	finally:
-		pass
+		return sonlinks
 
 
 # 页节点(边界)判定函数
@@ -61,35 +61,51 @@ def stop_flag(url):
 	if hostname == 'm.sohu.com':
 		return True
 	if hostname.startswith('m.') and hostname.endswith('sohu.com'):
-		# m.tv.sohu.com ...
+		# m.tv.sohu.com/m.mail.sohu.com ...
 		return True
 	return False
 
 
-class myThread (threading.Thread):
-    def __init__(self, threadID, name, counter):
+# 线程类
+class myThread(threading.Thread):
+    def __init__(self, threadID, queue, task, logger):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.name = name
-        self.counter = counter
-
+        self.queue = queue
+        self.task = task
+        self.logger = logger
 
     def run(self):
-        print "Starting " + self.name
-       # 获得锁，成功获得锁定后返回True
-       # 可选的timeout参数不填时将一直阻塞直到获得锁定
-       # 否则超时后将返回False
-        threadLock.acquire()
-        print_time(self.name, self.counter, 3)
-        # 释放锁
-        threadLock.release()
-		
+    	while not self.queue.empty():
+	    	url = self.queue.get()
+	        if url not in VISITED:
+	        	VISITED[url]=1
+	        	self.queue.task_done()
+	        else:
+	        	self.queue.task_done()
+	        	continue
+	        sonlinks = self.task(url,logger)
+	        for link in sonlinks:
+	        	self.queue.put(link)
+	        time.sleep(3)
 
 
 def main():
 	logger = getLogger(TARGET_URL)
 	WORKQUEUE.put(TARGET_URL)
+	threads=[]
+	for i in xrange(THREADNUM):
+		t = myThread(i,WORKQUEUE,bfs_path,logger)
+		t.daemon = True
+		t.start()
+		threads.append(t)
 
+	for thread in threads:
+		thread.join()
+
+	# 阻塞直到所有任务完成
+	WORKQUEUE.join()
+	print 'Job is done'
 
 if __name__ == '__main__':
 	main()
