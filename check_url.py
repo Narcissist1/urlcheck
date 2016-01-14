781a0c69ed81fad2a463b7212d089e0743577129
Narcissist1 <m6106918@gmail.com>
2016-01-15 02:20:56 +0800

    update

--- check_url.py
+++ check_url.py
@@ -15,9 +15,9 @@ import re
 
 TARGET_HOST = "http://m.sohu.com/"  # 目标url
 WORKQUEUE = Queue.Queue()  			# 待处理队列
-VISITED = {}  						# url是否处理过
-THREADNUM = 2  						# 线程数
-queueLock = threading.Lock()
+VISITED = {}  						# 已检测url集合
+THREADNUM = 100  					# 线程数
+queueLock = threading.Lock()		# 锁
 
 
 # 设置log文件
@@ -67,22 +67,23 @@ def stop_flag(url):
 # 广搜函数
 def bfs_path(url, logger):
     sonlinks = []
-    req = urllib2.Request(url)
+    headers = {"User-Agent": 'Mozilla 5.0'}
+    req = urllib2.Request(url.encode('utf-8'), headers=headers)
     try:
         response = urllib2.urlopen(req)
-        the_page = response.read()
+        the_page = response.read().decode('utf-8')
         # logger.info('page:%s' % the_page)
         soup = BeautifulSoup(the_page, 'html.parser')
         for link in soup.find_all('a', href=True):
             templink = urljoin(TARGET_HOST, link['href'])
             # logger.info('URl:%s' % templink)
-            if urlvalidator(templink) and stop_flag(templink):
+            if templink not in VISITED and urlvalidator(templink) and stop_flag(templink):
                 sonlinks.append(templink)
     except urllib2.HTTPError as e:
         # HTTPError 需要在前面
-        logger.error('HTTPError:' + str(e.code) + '\n' + str(e.read()) + url)
+        logger.error('HTTPError:' + str(e.code) + '-' + url)
     except urllib2.URLError as e:
-        logger.error('URLError:' + str(e.reason) + url)
+        logger.error('URLError:' + str(e.reason) + '-' +url)
     finally:
         return sonlinks
 
@@ -99,23 +100,25 @@ class myThread(threading.Thread):
 
     def run(self):
         print 'Thread-%d is Working...' % self.threadID
-        while not self.queue.empty():
-            queueLock.acquire()
-            url = self.queue.get()
-            if url not in VISITED:
-                VISITED[url] = 1
-                self.queue.task_done()
-            else:
-                self.queue.task_done()
-                queueLock.release()
-                continue
-            sonlinks = self.task(url, self.logger)
-            for link in sonlinks:
-                self.queue.put(link)
-            queueLock.release()
-            time.sleep(3)
-            print self.queue.qsize()
-		# print 'Thread-%d is done.' % self.threadID
+        while len(VISITED) < 1000:
+        	print 'Queue size %s' % self.queue.qsize()
+        	print 'VISITED size %s' % len(VISITED)
+	        if not self.queue.empty():
+	            url = self.queue.get()
+	            queueLock.acquire()
+	            if url not in VISITED:
+	                VISITED[url] = 1
+	                queueLock.release()
+	            else:
+	            	queueLock.release()
+	                self.queue.task_done()
+	                continue
+	            sonlinks = self.task(url, self.logger)
+	            for link in sonlinks:
+	                self.queue.put(link)
+	            self.queue.task_done()
+	        else:
+			    time.sleep(5)
 
 
 def main():
@@ -132,9 +135,10 @@ def main():
         thread.join()
 
     # 阻塞直到所有任务完成
-    WORKQUEUE.join()
+    # WORKQUEUE.join()
     print 'Job is done'
     print "checked %d urls" % len(VISITED)
+    logger.info("checked %d urls" % len(VISITED))
 
 if __name__ == '__main__':
     main()
